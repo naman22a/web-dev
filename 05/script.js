07433841006696b20ccb73092658f65fd010f5c3
Naman Arora <namanarora1022@gmail.com>
2024-10-27 15:08:19 +0530

    feat: 05

---
+++ 05/script.js
@@ -0,0 +1,21 @@
+let number = 23;
+document.getElementById('number').innerText = number;
+
+let string = 'Hello world';
+document.getElementById('string').innerText = string;
+
+let boolean = true;
+document.getElementById('boolean').innerText = boolean;
+
+let a = 2;
+let b = 3;
+
+let add = a + b;
+let sub = a - b;
+let mul = a * b;
+let div = a / b;
+
+document.getElementById('add').innerText = add;
+document.getElementById('sub').innerText = sub;
+document.getElementById('mul').innerText = mul;
+document.getElementById('div').innerText = div;
